771d99cba35dfb541e1542cdb16eff2368bd4d27
jrBMfh <Afn9Wz@OKdzhe.cc>
2021-04-30 09:21:33 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        nuxt-capacitor
+      </h1>
+      <div class="links"> {{networkStatus}} </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      networkStatus: ''
+    }
+  },
+  created(){
+    this.checkNetwork()
+    this.$network.addListener('networkStatusChange', (status) => {
+      this.networkStatus = status.connected ? 'online' : 'offline'
+    })
+    this.$toast.show({ text: 'I am a toast!' })
+  },
+  methods: {
+    async checkNetwork() {
+      this.networkStatus = (await this.$network.getStatus()).connected
+        ? 'online'
+        : 'offline'
+    },
+    showToast() {
+      this.$toast.show({ text: 'I am a toast!' })
+    },
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
