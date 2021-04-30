19667c5b80cbed45c5637d9d56c2adf4adcd4fda
jrBMfh <Afn9Wz@OKdzhe.cc>
2021-04-30 12:55:58 -0300

    add android

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,15 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    publicPath: '/assets/',
+    extend(config, { isDev, isClient }) {
+      if (!isDev) {
+        config.output.publicPath = './assets/'
+      }
+      return config
+    }
   },
+  
   pwa: {
     manifest: {
       name: 'Nuxt Capacitor Boilerplate',
